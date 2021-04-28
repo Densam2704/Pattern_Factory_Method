a8b0c13d887c98cd73ed2e392e405c62d1e116fb
Densam2704 <den.samoshckin@yandex.ru>
2021-04-28 20:51:52 +0300

    Added links

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,23 @@
 import Factory.Animal;
 import Factory.Rabbit;
 import Factory.Wolf;
+import Info.AnimalInfo;
+import Info.WolfInfo;
 
 public class Main {
 
-//  Inspired by https://refactoring.guru/ru/design-patterns/factory-method
+//  Inspired by
+//  https://refactoring.guru/ru/design-patterns/factory-method
+//  https://javarush.ru/groups/posts/2372--patternih-proektirovanija-factorymethod
   private static Animal animal;
   public static void main(String[] args) {
 	animal = new Rabbit();
 	animal.getAnimalName();
+	
+	animal = new Wolf();
+	animal.getAnimalName();
+	AnimalInfo info = animal.createInfo();
+	info.sayName();
 	
 	
   }
